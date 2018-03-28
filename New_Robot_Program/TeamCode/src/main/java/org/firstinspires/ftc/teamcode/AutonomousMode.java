c5c4fd83b198b5e23cfe92356cd6eea8b9b924e2
GAMEPC\Dani <damageq12@gmail.com>
2018-03-28 22:05:59 +0300

    Move forward with encoder function

--- New_Robot_Program/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMode.java
+++ New_Robot_Program/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMode.java
@@ -61,6 +61,123 @@ public abstract class AutonomousMode extends LinearOpMode {
     OpenGLMatrix lastLocation = null;
     VuforiaLocalizer vuforia;
 
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Map the motors
+        leftMotorF = hardwareMap.dcMotor.get("left_drive_front");
+        leftMotorB = hardwareMap.dcMotor.get("left_drive_back");
+        rightMotorF = hardwareMap.dcMotor.get("right_drive_front");
+        rightMotorB = hardwareMap.dcMotor.get("right_drive_back");
+        cubesMotor = hardwareMap.dcMotor.get("cubes");
+        // Map the servos
+        servoArm = hardwareMap.servo.get("arm");
+        servoColor = hardwareMap.servo.get("color");
+        servoCubesLeft = hardwareMap.servo.get("cubes_left");
+        servoCubesRight = hardwareMap.servo.get("cubes_right");
+        // Map the sensors
+        gyroSensor = (ModernRoboticsI2cGyro) hardwareMap.gyroSensor.get("gyro");
+        colorSensor = hardwareMap.get(ColorSensor.class, "color");
+        // Set the wheel motors
+        leftMotorF.setDirection(DcMotor.Direction.REVERSE);
+        leftMotorB.setDirection(DcMotor.Direction.REVERSE);
+        rightMotorF.setDirection(DcMotor.Direction.FORWARD);
+        rightMotorB.setDirection(DcMotor.Direction.FORWARD);
+        leftMotorF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftMotorB.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotorF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotorB.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        cubesMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        cubesMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        // Set the stopping method for wheels
+        leftMotorF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftMotorB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightMotorF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightMotorB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        cubesMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        // Set the cubes mechanism direction
+        cubesMotor.setDirection(DcMotor.Direction.FORWARD);
+        // Set servo directions
+        servoArm.setDirection(Servo.Direction.FORWARD);
+        servoColor.setDirection(Servo.Direction.FORWARD);
+        servoCubesLeft.setDirection(Servo.Direction.FORWARD);
+        servoCubesRight.setDirection(Servo.Direction.REVERSE);
+        // Set the motors power to 0
+        leftMotorF.setPower(0);
+        leftMotorB.setPower(0);
+        rightMotorF.setPower(0);
+        rightMotorB.setPower(0);
+        cubesMotor.setPower(0);
+        // Initialize servo positions
+        servoArm.setPosition(ARM_UP);
+        servoColor.setPosition(MID_SERVO);
+        servoCubesRight.setPosition(CUBES_MAX);
+        servoCubesLeft.setPosition(CUBES_MAX);
+
+        // Calibrate sensors
+        colorSensor.enableLed(true);
+        gyroSensor.calibrate();
+        while(!isStopRequested() && gyroSensor.isCalibrating() && opModeIsActive()){
+            idle();
+        }
+
+        telemetry.addData("Status", "Gyro calibrated");
+        telemetry.update();
+
+        waitForStart();
+
+        runOp();
+
+        endOp();
+    }
+
+    protected abstract void runOp();
+    protected abstract void endOp();
+
+    //**********
+    //Methods
+    //**********
+    protected void Power_Wheels(double LMotorsPower, double RMotorsPower){
+        leftMotorF.setPower(LMotorsPower);
+        leftMotorB.setPower(LMotorsPower);
+        rightMotorF.setPower(RMotorsPower);
+        rightMotorB.setPower(RMotorsPower);
+    }
+
+    protected void Move_Wheels_With_Encoders(double power, int target){
+
+        Range.clip(power, 0, 0.9);
+
+        leftMotorF.setTargetPosition(target);
+        leftMotorB.setTargetPosition(target);
+        rightMotorF.setTargetPosition(target);
+        rightMotorB.setTargetPosition(target);
+        leftMotorF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftMotorB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightMotorF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightMotorB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        Power_Wheels(power, power);
+
+        while(leftMotorF.isBusy() || leftMotorB.isBusy() || rightMotorF.isBusy() || rightMotorB.isBusy()){
+            idle();
+        }
+
+        Stop_Wheels();
+    }
+
+    protected void Rotate_Wheels_Gyro(double power, double direction, double gyrotarget){
+        Range.clip(gyrotarget, -180, 180);
+        Range.clip(direction, -1,1);
+        Range.clip(power, 0, 0.9);
+        int initHeading = gyroSensor.getHeading();
+
+        Power_Wheels(direction*power, direction*(-power) );
+
+    }
+
+    protected void Stop_Wheels(){
+        Power_Wheels(0, 0);
+    }
 
 }
